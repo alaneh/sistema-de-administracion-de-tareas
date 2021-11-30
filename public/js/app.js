1310a5fdc01af6cd43b4fe4d9b60d3dc13e4fe15
alaneh <aelizaldeh1900@alumno.ipn.mx>
2021-11-30 01:32:28 -0600

    Agregar frontend responsive

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 import listas from './modulos/listas';
 import tareas from './modulos/tareas';
 import { actualizarAvance } from './funciones/avance';
+import funcionModal from './funciones/funcionModal';
 document.addEventListener('DOMContentLoaded', () => {
     actualizarAvance();
 })
---
+++ public/js/funciones/funcionModal.js
@@ -0,0 +1,15 @@
+const openEls = document.querySelectorAll("[data-open]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+    el.addEventListener("click", function() {
+        const modalId = this.dataset.open;
+        document.getElementById(modalId).classList.add(isVisible);
+    });
+}
+
+document.addEventListener("click", e => {
+    if (e.target == document.querySelector(".modal.is-visible")) {
+        document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+});
